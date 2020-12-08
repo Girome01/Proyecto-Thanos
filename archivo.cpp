15379b490ccbacec176ab9ad29a90eee6317ac8e
Girome <gilrome2002@estudiantec.cr>
2020-12-08 17:57:56 -0600

    Agregue la struct para leer y escribir archivos txt

---
+++ archivo.cpp
@@ -0,0 +1,38 @@
+#include "archivo.h"
+
+Archivo::Archivo(){
+
+}
+
+void Archivo::escribir(string _texto, string _archivo){
+    ofstream archivo;
+
+    archivo.open(_archivo.c_str(),ios::out);
+
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo"<<endl;
+    }else{
+        archivo<<_texto<<endl;
+        cout<<"Se guardo el archivo"<<endl;
+    }
+    archivo.close();
+}
+
+string Archivo::leer(string _archivo){
+    ifstream archivo;
+    string texto="",linea;
+
+    archivo.open(_archivo.c_str(),ios::in);
+
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo"<<endl;
+    }else{
+        while(!archivo.eof()){
+            getline(archivo,linea);
+            texto += linea;
+        }
+        cout<<"Se leyo el archivo"<<endl;
+    }
+    archivo.close();
+    return texto;
+}
---
+++ archivo.h
@@ -0,0 +1,17 @@
+#ifndef ARCHIVO_H
+#define ARCHIVO_H
+
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+using namespace std;
+
+struct Archivo{
+    Archivo();
+
+    // _archivo tiene que llevar la extencion .txt
+    void escribir(string _texto, string _archivo);
+    string leer(string _archivo);
+};
+
+#endif // ARCHIVO_H
