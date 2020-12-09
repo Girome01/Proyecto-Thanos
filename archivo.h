2f00cc08fc3ae975110b79dd238ade045a411a25
Girome <gilrome2002@estudiantec.cr>
2020-12-09 11:12:18 -0600

    QT SIRVA POR DIOS

--- archivo.h
+++ archivo.h
@@ -9,7 +9,7 @@ using namespace std;
 struct Archivo{
     Archivo();
 
-    //Estoy harto de QT
+    //QT ES UNA MIERDA
     // _archivo tiene que llevar la extencion .txt
     void escribir(string _texto, string _archivo);
     string leer(string _archivo);
