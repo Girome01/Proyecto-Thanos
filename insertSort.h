cc262a853387a2378d28d310e88ecf5ba3a6cd90
Girome <gilrome2002@estudiantec.cr>
2020-12-17 20:49:00 -0600

    Incorpore el insert sort

---
+++ insertSort.h
@@ -0,0 +1,19 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+struct NodoPersona;
+struct ListaDoble;
+#include "listadoble.h"
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+struct insertSort{
+
+    insertSort();
+
+    void insertionSort(ListaDoble* lista,Persona* persona);
+};
+
+#endif // QUICKSORT_H
